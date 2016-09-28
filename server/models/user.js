cdd8bad63c1619197d088660eb41f108c2874091
Brian Doane <brianmdoane@gmail.com>
2016-09-28 11:02:13 -0500

    updated model.js schema, username, pw, info obj

--- server/models/user.js
+++ server/models/user.js
@@ -3,14 +3,18 @@
 const mongoose = require('mongoose')
 
 module.exports = mongoose.model('User', {
-	id: String,
-	name: String,
-	matches: [String],		
-	seen: [String],
-	interests: [String],
-	about: String,
-	email: String,
-	gender: String,		
-	genPref: String,
-	picture: String
+	username: String,
+	password: String,
+	info: {
+		id: String,
+		name: String,
+		matches: [String],		
+		seen: [String],
+		interests: [String],
+		about: String,
+		email: String,
+		gender: String,		
+		genPref: String,
+		picture: String
+	}	
 })
