fce6df40d495b3f761d37e363b935ddbdd68edb0
iamtylerd <tylerdanielphotography@gmail.com>
2016-09-28 10:23:24 -0500

    setup and structure

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// 'use strict';
+
+// const { json } = require('body-parser')
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose')
+// const routes = require('../server/routes/')
+
+
+// const port = process.env.PORT || 3000;
+// const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meantodo'
+
+
+// app.use(express.static('client'))
+// app.use(json())
+
+// // routes
+// app.use(routes)
+
+// //Listen
+// mongoose.Promise = Promise
+// mongoose.connect(MONGODB_URL, () =>
+// 	app.listen(port, () => console.log(`Listening on port: ${port}`))
+// 	)
