57d015069951e59e3d537e3a8f2bc305d2c0f382
Brian Doane <brianmdoane@gmail.com>
2016-09-28 11:59:12 -0500

    added database dir and file, installed redis, bcrypt, exp-session, connect into server.js

--- server/server.js
+++ server/server.js
@@ -6,23 +6,36 @@ const RedisStore = require('connect-redis')(session)
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const routes = require('../server/routes/')
-const { json } = require('body-parser')
-//const { connect } = require('./db/database')
+const bodyParser = require('body-parser')
+const { connect } = require('./database/database')
+
+// Instantiate app
 const app = express()
-
-
 const port = process.env.PORT || 3000;
-// const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meantodo'
 app.set('port', port)
+
+// Middlewares
+app.use(session({
+	store: new RedisStore(),
+	secret: 'carbondatemenow'
+}))
+
+app.use((req, res, next) => {
+	app.locals.username = req.session.username
+	next()
+})
+app.use(bodyParser.urlencoded({extended: false}))
 
 // app.use(express.static('client'))
 // app.use(json())
 
-// // routes
-// app.use(routes)
+// routes
+//app.use(routes)
 
-// //Listen
-// mongoose.Promise = Promise
-// mongoose.connect(MONGODB_URL, () =>
-app.listen(port, () => console.log(`Listening on port: ${port}`))
-// 	)
+//Listen
+connect()
+	.then(() =>
+		app.listen(port, () => {
+			console.log(`Listening on port: ${port}`)
+ 	}))
+ 	.catch(console.error)
