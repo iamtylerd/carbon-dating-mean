5ee5d01e84ec88a2812437b0bc67f31585102fb6
Brian Doane <brianmdoane@gmail.com>
2016-09-28 10:55:47 -0500

    server.js set up simple server, port listening

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-// 'use strict';
+'use strict'
 
-// const { json } = require('body-parser')
-// const express = require('express')
-// const app = express()
-// const mongoose = require('mongoose')
-// const routes = require('../server/routes/')
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('../server/routes/')
+const { json } = require('body-parser')
+const app = express()
 
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 // const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meantodo'
-
+app.set('port', port)
 
 // app.use(express.static('client'))
 // app.use(json())
@@ -20,5 +20,5 @@
 // //Listen
 // mongoose.Promise = Promise
 // mongoose.connect(MONGODB_URL, () =>
-// 	app.listen(port, () => console.log(`Listening on port: ${port}`))
+app.listen(port, () => console.log(`Listening on port: ${port}`))
 // 	)
