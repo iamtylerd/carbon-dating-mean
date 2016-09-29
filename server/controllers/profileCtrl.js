c5d3d2f6f2ff29f3279071e9ddb83a649c00137b
Eric Denton <eric@iamericanartist.com>
2016-09-29 10:10:21 -0500

    added profileCtrl and profileRoute

--- server/controllers/profileCtrl.js
+++ server/controllers/profileCtrl.js
@@ -3,5 +3,7 @@
 const User = require('../models/user')
 
 module.exports.userInfo = (req, res) => (res.json(req.session))
-
-
+module.exports.userMatches = (req, res) => {
+  console.log("req.session",req.session)
+  (res.json(req.session))
+}
--- server/routes/profileRoute.js
+++ server/routes/profileRoute.js
@@ -7,4 +7,6 @@ const profileCtrl = require('../controllers/profileCtrl')
 // when a post happens on /api/login, fire the imported loginCtrl.userLogin
 router.get('/api/profile', profileCtrl.userInfo)
 
+router.get('/api/profile/matches', profileCtrl.userMatches)
+
 module.exports = router
